7473c2adec714938976f7c3dae1bc6f43f0e5e43
KenDye <ken.w.dye@gmail.com>
2016-03-16 20:31:26 -0400

    this is a hell of a thing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Ken Dye
+## March 15, 2016
+## Data Science R Programming
+## Week 3 Programming project 2
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix creates a special Matrix containing a function to:
+## 1. Set the value of the Matrix
+## 2. Get the value of the Matrix
+## 3. Set the value of the inverse Matrix
+## 4. Get the value of the inverse Matrix
 
+makeCacheMatrix <- function(x = matrix()){
+        m <- NULL
+        set <- function(y){
+            x <<- y
+            m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSOlve:
+## 1. Computes the inverse of the special Matrix returned by makeCacheMatrix
+## 2. CHeck to see if inverse has already been solved, if solved and the Matrix
+##    has not changed, cacheSolve retrieves the inverse from cache and returns
+##    the result
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)){
+            message("Getting cached data.")
+            return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setinverse(m)4
+        m
 }
+
+
+
+
+
+
+
+
+
+
+
+
